9157a664b157ae77b31968c7c980eeef85cc3b58
Albert Panesse <albert.panesse@gmail.com>
2019-03-17 08:09:44 +0700

    CRUD users done

--- handlers/user.go
+++ handlers/user.go
@@ -6,6 +6,40 @@ import (
 	mdl "api-dapurhafi/models"
 )
 
+func (dbconn *DBConn) GetUsers(c *gin.Context) {
+	var (
+		users []mdl.User
+		result gin.H
+	)
+	
+	dbconn.DB.Find(&users)
+
+	result = gin.H{
+		"success": true,
+		"result": users,
+	}
+	
+	c.JSON(http.StatusOK, result)
+}
+
+func (dbconn *DBConn) GetUser(c *gin.Context) {
+	var (
+		user mdl.User
+		result gin.H
+	)	
+	
+	id := c.Param("id")
+	
+	dbconn.DB.First(&user, id)
+
+	result = gin.H{
+		"success": true,
+		"result": user,
+	}
+	
+	c.JSON(http.StatusOK, result)
+}
+
 func (dbconn *DBConn) CreateUser(c *gin.Context) {
 	var (
 		user mdl.User
@@ -15,8 +49,50 @@ func (dbconn *DBConn) CreateUser(c *gin.Context) {
 	c.BindJSON(&user)
 	
 	dbconn.DB.Create(&user)
+
 	result = gin.H{
+		"success": true,
 		"result": user,
+	}
+	
+	c.JSON(http.StatusOK, result)
+}
+
+func (dbconn *DBConn) UpdateUser(c *gin.Context) {
+	var (
+		oldUser, newUser mdl.User
+		result gin.H
+	)
+	
+	id := c.Param("id")
+
+	dbconn.DB.First(&oldUser, id)
+	
+	c.BindJSON(&newUser)	
+	
+	dbconn.DB.Model(&oldUser).Updates(&newUser)
+
+	result = gin.H{
+		"success": true,
+		"result": oldUser,
+	}
+	
+	c.JSON(http.StatusOK, result)
+}
+
+func (dbconn *DBConn) DeleteUser(c *gin.Context) {
+	var (
+		user mdl.User
+		result gin.H
+	)
+	
+	id := c.Param("id")
+
+	dbconn.DB.First(&user, id)
+	dbconn.DB.Delete(&user)
+
+	result = gin.H{
+		"success": true,
 	}
 	
 	c.JSON(http.StatusOK, result)
--- main.go
+++ main.go
@@ -15,7 +15,11 @@ func main() {
 
 	router := gin.Default()
 
+	router.GET("/users", dbconn.GetUsers)
+	router.GET("/user/:id", dbconn.GetUser)
 	router.POST("/user", dbconn.CreateUser)
+	router.PUT("/user/:id", dbconn.UpdateUser)
+	router.DELETE("/user/:id", dbconn.DeleteUser)
 	
 	log.Fatal(router.Run(":3000"))
 }
